4f531127ee6fd0e5f6ac4461af1dc00f985f90af
mangeot <mangeot@ca60a4fc-6ab6-48d5-a899-f71ac0f768e1>
2012-11-24 12:28:37 +0000

    lang menu bf
    git-svn-id: svn+ssh://scm.forge.imag.fr/var/lib/gforge/chroot/scmrepos/svn/ipotruc/trunk/ipolex@9 ca60a4fc-6ab6-48d5-a899-f71ac0f768e1

--- include/language_negociation.php
+++ include/language_negociation.php
@@ -61,16 +61,16 @@ echo '
         <img src="',RACINE_WEB,'images/multilingual_logo.png" alt="lang/语言/言語/لُغة/..." height="20px" style="vertical-align: middle;"/>
          <select name="lang" id="lang" onchange="this.form.submit()">
           <option label="lang go" selected="selected" value="">lang/语言/لُغة/...</option>
-          <!--option label="arabiya" value="ara">عربية</option>
-          <option label="Catala" value="cat">Català</option>
-          <option label="Deutsch" value="deu">Deutsch</option-->
-          <option label="English" value="eng">English</option>
-          <!--option label="espanol" value="esp">español</option-->
-          <option label="francais" value="fra">français</option>
-          <!--option label="nihongo" value="jpn">日本語</option>
-          <option label="Melayu" value="msa">Melayu</option>
-          <option label="Russian" value="rus">Русский</option>
-          <option label="jiantizhongwen" value="zho">简体中文</option-->
+          <!--option value="ara">عربية</option>
+          <option  value="cat">Català</option>
+          <option value="deu">Deutsch</option-->
+          <option value="eng">English</option>
+          <!--option value="esp">español</option-->
+          <option value="fra">français</option>
+          <!--option value="jpn">日本語</option>
+          <option value="msa">Melayu</option>
+          <option value="rus">Русский</option>
+          <option value="zho">简体中文</option-->
         </select>
         <noscript><div><input type="submit" value="Go" /></div></noscript>
       </div>
