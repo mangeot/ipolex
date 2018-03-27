914a9ba4700d165f2c6190d147a4feaa6be822fb
Mathieu Mangeot <Mathieu.Mangeot@imag.fr>
2018-03-27 23:57:00 +0200

    Copie récursive des noeud de départ dans l'arbre d'arrivée À tester !

--- transformation/transformation-fichiercomplet.pl
+++ transformation/transformation-fichiercomplet.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# ./transformation-fichiercomplet.pl -i Donnees/anaan.xml -o 'Thierno' -m Donnees/Baat_fra-wol/Baat_wol_fra-metadata.xml -s Donnees/Baat_fra-wol/DicoArrivee_wol_fra-metadata.xml -t Donnees/Baat_fra-wol/dicoarrivee_wol_fra-template.xml > out.xml
+# ./transformation-fichiercomplet.pl -i Donnees/anaan.xml -n 'Thierno' -m Donnees/Baat_fra-wol/Baat_wol_fra-metadata.xml -s Donnees/Baat_fra-wol/DicoArrivee_wol_fra-metadata.xml -t Donnees/Baat_fra-wol/dicoarrivee_wol_fra-template.xml > out.xml
 # ./transformation-fichiercomplet.pl -i Donnees/Baat_fra-wol/baat_wol_fra-prep.xml  -m Donnees/Baat_fra-wol/Baat_wol_fra-metadata.xml -n Donnees/Baat_fra-wol/DicoArrivee_wol_fra-metadata.xml -t Donnees/Baat_fra-wol/dicoarrivee_wol_fra-template.xml > out.xml
 #
 #
@@ -32,18 +32,24 @@ GetOptions(
   );
  
 my $date = localtime;
+my $INFILE;
 my $OUTFILE;
-if (!(defined $fichierEntree)) {
-	$fichierEntree  = *STDIN;
+
+if ($fichierEntree) {
+	open $INFILE, "<:encoding($encoding)",$fichierEntree or die ("$! $fichierEntree \n");
 } # si le fichier entree n'est pas spécifié, on ouvre l'entrée standard
-if (!(defined $fichierSortie)) {
-	$OUTFILE = *STDOUT;
+else {
+	$INFILE  = *STDIN;
+}
+if ($fichierSortie) {
+	open $OUTFILE, ">:encoding($unicode)",$fichierSortie or die ("$! $fichierSortie \n");
 } # si le fichier sortie n'est pas spécifié, on ouvre la sortie standard
 else {
-	open $OUTFILE, ">:encoding($unicode)",$fichierSortie or die ("$! $fichierSortie \n");
+	$OUTFILE = *STDOUT;
 }
 if (! ($entreeModele && $metaEntree && $metaSortie)) {&help;};
 if (defined $help) {&help;};
+
 
 binmode(STDERR, ":utf8");
 
@@ -52,21 +58,19 @@ sub help {
 	exit 0;
 }
 
-open my $INFILE, "<:encoding($encoding)",$fichierEntree or die ("$! $fichierEntree \n");
-
+# print STDERR "Chargement du modèle\n";
 open my $MODELEFILE, "<:encoding($unicode)", $entreeModele or die "error opening $entreeModele: $!";
+my $xmlarrivee = do { local $/; <$MODELEFILE> };
+close $MODELEFILE;
+#print STDERR "XMLarrivée : [",$xmlarrivee,"]",$entreeModele;
 
-#print STDERR "load cdm depart:\n";
+#print STDERR "load cdm départ:\n";
 my %CDMSDEPART=load_cdm($metaEntree);
-#print STDERR "load cdm arrivee:\n";
+#print STDERR "load cdm arrivée:\n";
 my %CDMSARRIVEE=load_cdm($metaSortie);
 
 
-
-my $xmlarrivee = do { local $/; <$MODELEFILE> };
-
-#print STDERR "XMLarrivée : [",$xmlarrivee,"]",$entreeModele;
-
+# print STDERR "Récupération de quelques pointeurs CDM utiles pour la suite\n";
 my $cdmvolumedepart = delete($CDMSDEPART{'cdm-volume'});
 my $cdmvolumearrivee = delete($CDMSARRIVEE{'cdm-volume'});
 
@@ -74,28 +78,36 @@ my $cdmentrydepart = delete($CDMSDEPART{'cdm-entry'});
 my $cdmentryarrivee = delete($CDMSARRIVEE{'cdm-entry'});
 
 my $cdmheadworddepart = $CDMSDEPART{'cdm-headword'};
-print STDERR "cdm : $cdmheadworddepart\n";
 
+my $CDMArbreDepart = arbre_cdm(\%CDMSDEPART);
+my $CDMArbreArrivee = arbre_cdm(\%CDMSARRIVEE);
+
+print STDERR "arbredepart: \n",Dumper($CDMArbreDepart);
+print STDERR "arbrearrivee: \n",Dumper($CDMArbreArrivee);
+
+# On reconstruit les balises ouvrantes et fermantes du volume 
 my $headerdepart = xpath2opentag($cdmvolumedepart);
 my $footerdepart = xpath2closedtag($cdmvolumedepart);
-
 
 my $closedtagentryarrivee = xpath2closedtag(xpathdifference($cdmentrydepart,$cdmvolumedepart));
 my $opentagvolumearrivee = xpath2opentag($cdmvolumearrivee, 'creation-date="' . $date . '"');
 my $closedtagvolumearrivee = xpath2closedtag($cdmvolumearrivee);
 
+# On va lire le fichier d'entrée article par article 
+# donc on coupe après une balise de fin d'article.
 $/ = $closedtagentryarrivee;
 
 my $parser= XML::DOM::Parser->new();
 
+# On imprime le début du fichier résultat = en-tête XML
 print $OUTFILE '<?xml version="1.0" encoding="UTF-8" ?>
 ';
 print $OUTFILE $opentagvolumearrivee,"\n";
 
+# boucle principale sur chaque entrée
 while( my $line = <$INFILE>)  {   
 
 	$line = $headerdepart . $line . $footerdepart;
-
 	my $docdepart = $parser->parse ($line);
 	my $docarrivee = $parser->parse ($xmlarrivee);
 	
@@ -103,30 +115,24 @@ while( my $line = <$INFILE>)  {
 	my $headword = getNodeText($headwords[0]);
 	print STDERR "Transformation article : $headword\n";
 
-	
-	foreach my $pointeur (keys %CDMSDEPART) {
-    	my $cdmdepart = $CDMSDEPART{$pointeur};
-    	my $cdmarrivee = $CDMSARRIVEE{$pointeur};
-    	if ($cdmarrivee) {
-			&copiePointeurs($cdmdepart, $cdmarrivee, $docdepart, $docarrivee);
-    	}
-	}
+	copiePointeurs($CDMArbreDepart, $CDMArbreArrivee, $docdepart, $docarrivee);
 
-#	print STDERR "fin des copiePointeurs\n";
+	#	print STDERR "fin des copiePointeurs\n";
 
-my @entryarrivee = $docarrivee->findnodes($cdmentryarrivee);
-my $entryarrivee = $entryarrivee[0];
+	my @entryarrivee = $docarrivee->findnodes($cdmentryarrivee);
+	my $entryarrivee = $entryarrivee[0];
 
-# copie de l'entrée source dans l'entrée arrivée
+	# print STDERR "copie de l'entrée source dans l'entrée arrivée\n";
 
-my @entrydepart = $docdepart->findnodes($cdmentrydepart);
-my $entrydepart = $entrydepart[0];
+	my @entrydepart = $docdepart->findnodes($cdmentrydepart);
+	my $entrydepart = $entrydepart[0];
 
-my $elementsource = $docarrivee->createElement('entree-source');
-$elementsource->setAttribute('provenance',$nomDicoDepart);
-my $cloneEntrydepart = $entrydepart->cloneNode(1);
-$cloneEntrydepart->setOwnerDocument($docarrivee);
-$elementsource->appendChild($cloneEntrydepart);
+#	print STDERR "Recopie de l'article de départ tel quel dans l'article d'arrivée pour éventuel travail ultérieur\n";
+	my $elementsource = $docarrivee->createElement('entree-source');
+	$elementsource->setAttribute('provenance',$nomDicoDepart);
+	my $cloneEntrydepart = $entrydepart->cloneNode(1);
+	$cloneEntrydepart->setOwnerDocument($docarrivee);
+	$elementsource->appendChild($cloneEntrydepart);
 	$entryarrivee->appendChild($elementsource);
 	print $OUTFILE $entryarrivee->toString,"\n";
 #	print STDERR "Fin transformation article\n";
@@ -135,7 +141,10 @@ $elementsource->appendChild($cloneEntrydepart);
 print $OUTFILE $closedtagvolumearrivee;
 
 
-sub xpathdifference{
+
+
+# Cette fonction permet de calculer la différence entre deux XPath
+sub xpathdifference {
 	my $xpath = $_[0];
 	my $xpathcourt = $_[1];
 	$xpath =~ s/\/$//;
@@ -149,6 +158,7 @@ sub xpathdifference{
 	return $xpath;
 }
 
+# Cette fonction convertit un XPath en balises ouvrantes
 sub xpath2opentag {
 	my $xpath = $_[0];
 	my $attribut = $_[1] || '';
@@ -161,6 +171,7 @@ sub xpath2opentag {
 	$xpath .= $attribut . '>';
 }
 
+# Cette fonction convertit un XPath en balises fermantes
 sub xpath2closedtag {
 	my $xpath = $_[0];
 	my $tags = '';
@@ -173,6 +184,7 @@ sub xpath2closedtag {
 	return $tags;
 }
 
+# Cette fonction permet de récupérer le texte dans un nœud DOM quel que soit le type de nœud
 sub getNodeText {
 	my $node = $_[0];
 	my $text = '';
@@ -200,9 +212,7 @@ sub getNodeText {
 }
 
 
-#cette fonction permet de récupérer les pointeurs cdm à partir du fichier metada.
-
-
+# cette fonction permet de récupérer les pointeurs cdm à partir du fichier metada.
 sub load_cdm {
   my ($fichier)=@_;
   open (IN, "<:encoding($unicode)", $fichier);
@@ -219,42 +229,119 @@ sub load_cdm {
   return %dico;
 }
 
+# Cette fonction copie le résultat d'un pointeur XPath de départ dans un résultat de pointeur XPath d'arrivée
+# Elle est récursive
 sub copiePointeurs {
-	my $pointeurDepart = $_[0];
-	my $pointeurArrivee = $_[1];
-	my $docDepart = $_[2];
-	my $docArrivee = $_[3];
+	my %ArbreDepart = %{ shift() };
+	my %ArbreArrivee = %{ shift() };
+	my $ancetreDepart = shift();
+	my $ancetreArrivee = shift();
 	
-	# ATTENTION : supprimer le / final sinon le module xpath bugue !
-	$pointeurDepart =~ s/\/$//;
-	$pointeurArrivee =~ s/\/$//;
-	$pointeurArrivee =~ s/\/text\(\)$//;
-	print STDERR 'cdmd: ', $pointeurDepart, ' cdma: ', $pointeurArrivee,"\n";
-	my @valeurs = $docDepart->findnodes($pointeurDepart);
+	foreach my $cle (keys %ArbreDepart) {
+		my $pointeurDepart = $ArbreDepart{$cle};
+		my @pointeursDepart = @$pointeurDepart;
+		$pointeurDepart = $pointeursDepart[0];
+		my $pointeurArrivee = $ArbreArrivee{$cle};
+		if ($pointeurArrivee) {
+			my @pointeursArrivee = @$pointeurArrivee;
+			$pointeurArrivee = $pointeursArrivee[0];
+		
+			# ATTENTION : supprimer le / final sinon le module xpath bugue !
+			$pointeurDepart =~ s/\/$//;
+			$pointeurArrivee =~ s/\/$//;
+			$pointeurArrivee =~ s/\/text\(\)$//;
 
-	my @noeudsArrivee = $docArrivee->findnodes($pointeurArrivee);
-	my $noeudArrivee = $noeudsArrivee[0];
+			my @noeudsArrivee = $ancetreArrivee->findnodes($pointeurArrivee);
+			my $noeudArrivee = $noeudsArrivee[0];
 
-	if (!$noeudArrivee) {
-		print STDERR "$pointeurArrivee gives null value\n";
-	}
-	else {
-		my $noeudParent = $noeudArrivee->getParentNode();
-		my $noeudSuivant = $noeudArrivee->getNextSibling();
-		if (scalar(@valeurs)>0) 	{$noeudParent->removeChild($noeudArrivee);}
-		foreach my $valeur (@valeurs) {
-			my $noeudTexte = getNodeText($valeur);
-			my $noeudClone = $noeudArrivee->cloneNode(1);
-			$noeudClone->setOwnerDocument($docArrivee);
-			$noeudClone->addText($noeudTexte);
-			# si la variante a un noeud suivant
-			if ($noeudSuivant) {
-				$noeudParent->insertBefore($noeudClone,$noeudSuivant);
+			if ($noeudArrivee) {
+				print STDERR 'cdmd: ', $pointeurDepart, ' cdma: ', $pointeurArrivee,"\n";
+				my $noeudArriveeParent = $noeudArrivee->getParentNode();
+				my $noeudArriveeSuivant = $noeudArrivee->getNextSibling();
+				my @noeudsDepart = $ancetreDepart->findnodes($pointeurDepart);
+				if (scalar(@noeudsDepart)>0) 	{$noeudArriveeParent->removeChild($noeudArrivee);}
+				foreach my $noeudDepart (@noeudsDepart) {
+					my $noeudClone = $noeudArrivee->cloneNode(1);
+					$noeudClone->setOwnerDocument($noeudArrivee->getOwnerDocument());
+					if (scalar(@pointeursDepart)>1 && scalar(@pointeursArrivee)>1) {
+						my $descendantsDepart = $pointeursDepart[1];
+						my $descendantsArrivee = $pointeursArrivee[1];
+#						print STDERR "Appel récursif : copiePointeurs\n";
+						copiePointeurs($descendantsDepart,$descendantsArrivee,$noeudDepart,$noeudClone);
+					}
+					else {
+						my $noeudTexte = getNodeText($noeudDepart);
+						print STDERR "noeudTexte: $noeudTexte\n";
+						$noeudClone->addText($noeudTexte);
+					}
+					# si la variante a un noeud suivant
+					if ($noeudArriveeSuivant) {
+						$noeudArriveeParent->insertBefore($noeudClone,$noeudArriveeSuivant);
+					}
+					else { # sinon
+						$noeudArriveeParent->appendChild($noeudClone);
+					}
+				}
 			}
 			else {
-			# sinon
-			$noeudParent->appendChild($noeudClone);
+	#			print STDERR "$noeudArrivee gives null value\n";
 			}
 		}
+		else {
+#			print STDERR "$pointeurArrivee gives null value\n";
+		}
+	}	
+}
+
+# Cette fonction transforme un tableau de pointeurs CDM en arbre de pointeurs
+# les pointeurs enfants sont des descendants de l'arbre XML
+sub arbre_cdm {
+	my $tableauDepart = $_[0];
+#	print STDERR Dumper($tableauDepart);
+	my @keys = reverse sort { $tableauDepart->{$a} cmp $tableauDepart->{$b} } keys %{ $tableauDepart };
+#	print STDERR Dumper(\@keys);
+	foreach my $key (keys %{ $tableauDepart }) {
+		my $pointeur = $tableauDepart->{$key};
+		my @feuille = ( $pointeur );
+		$tableauDepart->{$key} = \@feuille;
+	}	
+	my $i=0;
+	my $keyssize = scalar(@keys);
+	foreach my $firstkey (@keys) {
+#		print STDERR "FK: $firstkey\n";
+		my $fpointeur = $tableauDepart->{$firstkey};
+		my @fpointeur = @$fpointeur;
+		$fpointeur = $fpointeur[0];
+		my $j=$i+1;
+		while ($j<$keyssize) {
+			my $secondkey = $keys[$j];
+#			print STDERR "$firstkey , $secondkey\n";
+			my $spointeur = $tableauDepart->{$secondkey};
+			my @spointeur = @$spointeur;
+			$spointeur = $spointeur[0];
+			if ($fpointeur =~ s/^\Q$spointeur\E/\./) {
+				my %hash = ();
+				if (scalar (@spointeur)>1) {
+					my $hash = $spointeur[1];
+					%hash = %$hash;
+				}
+				if (scalar(@fpointeur)>1) {
+					@fpointeur = ($fpointeur,$fpointeur[1]);
+				}
+				else {
+					@fpointeur = ($fpointeur);
+				}
+				$hash{$firstkey} = \@fpointeur;
+				@spointeur = ($spointeur, \%hash);
+				$tableauDepart->{$secondkey} = \@spointeur;
+				delete ($tableauDepart->{$firstkey});
+				$j = $keyssize;
+			}
+			else {
+				$j++;
+			}
+		}
+		$i++;
 	}
+	return $tableauDepart;
 }
