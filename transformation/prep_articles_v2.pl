73524d19bc0d60c09dbc1ab52871fae4496b5e5a
Mathieu Mangeot <Mathieu.Mangeot@imag.fr>
2018-05-27 19:55:19 +0200

    Nouvelle version de prep_articles

---
+++ transformation/prep_articles_v2.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+
+use XML::XPath;
+use XML::XPath::XMLParser;
+use utf8::all;
+
+my $entry=$ARGV[1];
+
+my $xp = XML::XPath->new(filename => $ARGV[0]);
+ 
+my $nodeset = $xp->find($ARGV[1]); # find all paragraphs
+ 
+foreach my $node ($nodeset->get_nodelist) {
+	my $entry = XML::XPath::XMLParser::as_string($node);
+	$entry =~ s/\R//gsm;
+    print STDOUT $entry, "\n";
+}
